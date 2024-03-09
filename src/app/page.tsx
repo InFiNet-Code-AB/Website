2c4db8f54b695f916d3e46da1837f13afb719c0a
Nemanja1208 <nevaljao994@gmail.com>
2024-03-09 23:57:40 +0100

    nemanja/ wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,9 @@
 import { NavigationMenu } from "@/components/Navigation/Navigation";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="dark dark:my-dark-bg">
       <NavigationMenu />
-      <div className="flex justify-center items-center min-h-screen">
-        <div className="flex flex-col items-center justify-between p-4 md:p-8 lg:p-24">
-          <div className="relative flex flex-col items-center justify-center">
-            <Image
-              className="dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-full h-auto md:h-full"
-              src="/underconstruction.webp"
-              alt="Next.js Logo"
-              width={800}
-              height={600}
-              priority
-            />
-            <div className="text-center text-2xl md:text-4xl font-semibold text-white mt-4">
-              Coming Soon, Work in Progress
-            </div>
-          </div>
-          <div className="mb-8 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationMenuItems } from "./NavigationItems";
 import { DarkMode } from "./ToggleDarkMode";
 
@@ -11,18 +11,41 @@ import { Drawer, DrawerTrigger } from "../shadcn/Drawer";
 import { Button } from "../shadcn/Button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { MobileNavigationItems } from "./MobileNavigation/MobileNavigationItems";
+import { useTheme } from "next-themes";
 
 export const NavigationMenu = () => {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    console.log("here");
+    renderLogoBasedOnTheme(theme);
+  }, [theme]);
+
+  const renderLogoBasedOnTheme = (theme: string | undefined) => {
+    console.log(theme);
+    if (theme === "dark")
+      return (
+        <Image
+          src="/logo-dark-long.png"
+          alt="InFiNet Code AB logo"
+          width={150}
+          height={50}
+        />
+      );
+    return (
+      <Image
+        src="/logo-light-long.png"
+        alt="InFiNet Code AB logo"
+        width={150}
+        height={50}
+      />
+    );
+  };
   if (isMobile) {
     return (
       <div className="flex justify-between">
         <div className="h-14 w-3/4 justify-center align-top">
-          <Image
-            src="/logo-dark-long.png"
-            alt="InFiNet Code AB logo"
-            width={150}
-            height={50}
-          />
+          {renderLogoBasedOnTheme(theme)}
         </div>
         <div className="justify-end h-14">
           <Drawer>
